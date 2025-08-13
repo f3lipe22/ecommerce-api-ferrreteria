4477e511fc639526ceec53202d09c1ae3c8a6461
f3lipe22 <andresfelipediaz782@gmail.com>
2025-08-13 16:54:25 -0500

    Mi primer commit

---
+++ src/main/java/com/example/ecommer_api/EcommerApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.ecommer_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcommerApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcommerApiApplication.class, args);
+	}
+
+}
